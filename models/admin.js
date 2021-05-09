09a10bfe6a55e40c70fa175cb717d34f5f16483c
Álvaro Aguerré Navarrete <alvaro78an@gmail.com>
2021-05-09 19:42:54 +0200

    Modify admin model

--- models/admin.js
+++ models/admin.js
@@ -3,22 +3,29 @@ const {
   Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class Admin extends Model {
-    
-    static associate(models) {
-      this.hasMany(models.Product, {
-        foreignKey : "adminId",
-        sourceKey: "id"
-      })
-    }
-  };
-  Admin.init({
-    name: DataTypes.STRING,
-    email: DataTypes.STRING,
-    password: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'Admin',
-  });
-  return Admin;
+    class Admin extends Model {
+        
+        static associate(models) {
+
+        //     this.hasMany(models.Product, {
+        //       foreignKey: "adminId",
+
+        //     });
+      
+        //     this.hasMany(models.Customer, {
+        //       foreignKey: "adminId",
+        //     })
+          }
+    };
+
+    Admin.init({
+        name: DataTypes.STRING,
+        email: DataTypes.STRING,
+        password: DataTypes.STRING
+    }, {
+        sequelize,
+        modelName: 'Admin',
+    });
+    return Admin;
+
 };

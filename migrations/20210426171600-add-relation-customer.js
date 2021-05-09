c2f21c6c51ef6447617365859d3ff4916b04cd19
Álvaro Aguerré Navarrete <alvaro78an@gmail.com>
2021-05-09 19:44:53 +0200

    Add relation customer migration

--- migrations/20210426171600-add-relation-customer.js
+++ migrations/20210426171600-add-relation-customer.js
@@ -3,31 +3,44 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
 
-    return [queryInterface.addColumn(
-      'Customers', 
-      'cart_rootId', 
-      {
-          type: Sequelize.INTEGER,
-          references: {
-              model: 'Cart_roots', 
-              key: 'id' 
-          },
-          onUpdate: 'CASCADE',
-          onDelete: 'SET NULL'
-      }),
-      queryInterface.addColumn(
-        'Customers', 
-        'orderId', 
-        {
-            type: Sequelize.INTEGER,
-            references: {
-                model: 'Orders', 
-                key: 'id' 
-            },
-            onUpdate: 'CASCADE',
-            onDelete: 'SET NULL'
-        }),
-    ];
+    return [
+      
+    //   queryInterface.addColumn(
+    //     'AdminId', 
+    //     {
+    //         type: Sequelize.INTEGER,
+    //         references: {
+    //             model: 'Admins', 
+    //             key: 'id' 
+    //         },
+    //         onUpdate: 'CASCADE',
+    //         onDelete: 'SET NULL'
+    //     }),
+        queryInterface.addColumn(
+            'Customers', 
+            'cartId', 
+            {
+                type: Sequelize.INTEGER,
+                references: {
+                    model: 'Carts', 
+                    key: 'id' 
+                },
+                onUpdate: 'CASCADE',
+                onDelete: 'SET NULL'
+            }),
+        queryInterface.addColumn(
+            'Customers', 
+            'orderId', 
+            {
+                type: Sequelize.INTEGER,
+                references: {
+                    model: 'Orders', 
+                    key: 'id' 
+                },
+                onUpdate: 'CASCADE',
+                onDelete: 'SET NULL'
+            }),
+        ];
   },
 
   down: async (queryInterface, Sequelize) => {

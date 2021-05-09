7e989dc264911091e5ca3489280ad6ebd740b9e5
Álvaro Aguerré Navarrete <alvaro78an@gmail.com>
2021-05-09 08:31:03 +0200

    Add product router

--- router.js
+++ router.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const customerRouter = require('./routes/customer.router');
 const adminRouter = require('./routes/admin.router');
+const productRouter = require('./routes/product.router');
 
 router.use('/customers', customerRouter);
 router.use('/admins', adminRouter);
+router.use('/products', productRouter);
 
 module.exports = router;
